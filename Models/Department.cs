82985df0b1a5ae2047052b3a10e62442ca1bd9d6
Abdallah Elnoty <64426994+elnoty@users.noreply.github.com>
2021-01-13 18:47:53 -0800

    Add files via upload

---
+++ Models/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCday4.Models
+{
+    public class Department
+    {
+        public int ID { set; get; }
+        [Display(Name = "Department name")]
+        [Required]
+        public string Name { set; get; }
+        
+        //Navigation property
+        public List<Student> student { set; get; }
+
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCday4.Models
+{
+    public class Student
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Student name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Range(18, 30)]
+        public int Age { get; set; }
+        [Required(ErrorMessage = "*")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "*")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "*")]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string ConfigPassword { get; set; }
+
+        //Add a foreign key
+        [ForeignKey("Department")]
+        public int DeptID { get; set; }
+
+        //Navigational propartiy
+        public Department Department { get; set; }
+
+    }
+}
